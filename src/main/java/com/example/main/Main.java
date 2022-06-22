d767c7184f860561fc81f7cc8d6186fb0c0a3623
hello-github-ui <util.you.come@gmail.com>
2022-06-22 18:41:52 +0800

    测试 目录列表获取 上传 下载 功能

--- src/main/java/com/example/main/Main.java
+++ src/main/java/com/example/main/Main.java
@@ -2,7 +2,6 @@ package com.example.main;
 
 import com.example.config.SftpConfig;
 import com.example.util.SFTPUtil;
-import com.jcraft.jsch.SftpException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +15,10 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+
+        // 获取 args 参数  0: 列出某个目录下的文件  1: 下载某个目录下的文件  2: 上传某个文件到服务器
+//        String runType = args[0];
+
         SFTPUtil ftp = new SFTPUtil(3, 6000);
         try {
             // 加载配置文件信息
@@ -40,10 +43,17 @@ public class Main {
 
             SftpConfig sftpConfig = new SftpConfig(ip, Integer.parseInt(port), username, password, Integer.parseInt(timeout), remoteDir);
 
+            // 列出目录下的文件
             List<String> list = ftp.listFiles("/home/bigdata/bin/", sftpConfig);
-            logger.info("{} 目录下的文件列表: {}", remoteDir, new Object[]{list});
-            list.forEach(System.out::println);
-        } catch (SftpException e) {
+
+            // 下载文件 测试通过
+//            ftp.download(remoteDir, "xcall.sh", "D:\\temp\\", sftpConfig);
+
+            // 上传文件 测试通过
+//            ftp.upload(remoteDir, "D:\\temp\\1.png", sftpConfig);
+
+            logger.info("{} 目录下的文件列表: \n{}", remoteDir, new Object[]{list});
+        } catch (Exception e) {
             logger.error("文件上传下载异常:[{}]", e.getMessage());
         }
     }
